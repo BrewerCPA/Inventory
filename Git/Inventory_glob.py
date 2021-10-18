12691f56cbd5912b29171f36f4c5a3e22ce45888
BrewerCPA <chase.cribbet@outlook.com>
2021-10-18 17:34:20 -0500

    "Deleted comments"

--- Git/Inventory_glob.py
+++ Git/Inventory_glob.py
@@ -10,6 +10,7 @@ import glob
 import re
 
 
+
 path = r'C:\Users\chase\OneDrive\Desktop\Balance Sheet\Inventory\Ekos Inventory Analysis\Ekos Inv Downloads\[0-9]*.[0-9]*'
 
 def get_files(path): 
@@ -18,20 +19,10 @@ def get_files(path):
     return files
 
 
-# for i in range(2019,2022): 
-#     for j in range(1, 13):
-        # year = i
-        # month = j
-        # day = calendar.monthrange(year, month)[1]
-        # print(f'{year}.{month}.{day}')
-        
-
 def file_dates(file):
     year = int(re.search(r'(\d+)', file).group())
     month = int(re.search(r'\.(\d\d)', file).group()[1:])
     day = calendar.monthrange(year, month)[1]
        
-        
     return year, month, day
         
-    
